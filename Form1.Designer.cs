451422ba1dd9dda13e9d725ac878026258102549
hp <hp@DESKTOP-GSULJKB>
2023-11-06 16:03:52 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Unidad_8_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Factorial de un numero:";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(188, 87);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(90, 109);
+            listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(178, 48);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(312, 68);
+            button1.Name = "button1";
+            button1.Size = new Size(78, 36);
+            button1.TabIndex = 3;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 225);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Factorial del numero";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(250, 225);
+            label3.Name = "label3";
+            label3.Size = new Size(18, 15);
+            label3.TabIndex = 5;
+            label3.Text = "es";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(144, 222);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(274, 222);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(411, 330);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
